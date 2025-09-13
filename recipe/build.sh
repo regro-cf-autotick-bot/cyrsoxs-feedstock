e7360933bd4cd53d8d00f77622eaec78ca48b0a6
Dean M. DeLongchamp <dean.delongchamp@nist.gov>
2025-09-13 16:54:59 -0400

    attempting patch to rewrite cmake target architectures

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,113 @@
-set -exuo pipefail
+# --- Base (single precision, exe) ---
+mkdir build
+cd build
+cmake ${SRC_DIR} \
+  ${CMAKE_ARGS} \
+  -DCMAKE_BUILD_TYPE=Release 
+make install
+cd ../
+rm -r build
 
+mkdir build-pybind
+cd build-pybind
+cmake ${SRC_DIR} \
+  ${CMAKE_ARGS} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DPYBIND=Yes \
+  -DUSE_SUBMODULE_PYBIND=No \
+  -DPython_EXECUTABLE="$PYTHON"  \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  ${CMAKE_PLATFORM_FLAGS[@]} \
+  -DCMAKE_CUDA_RUNTIME_LIBRARY=Shared
+make install
+cd ../
+rm -r build-pybind 
+
+mkdir build
+cd build
+cmake ${SRC_DIR} \
+  ${CMAKE_ARGS} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DDOUBLE_PRECISION=Yes \
+  -DOUTPUT_BASE_NAME=CyRSoXS_dbl
+make install
+cd ../
+rm -r build 
+
+mkdir build-pybind
+cd build-pybind
+cmake ${SRC_DIR} \
+  ${CMAKE_ARGS} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DPYBIND=Yes \
+  -DUSE_SUBMODULE_PYBIND=No \
+  -DPython_EXECUTABLE="$PYTHON"  \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  ${CMAKE_PLATFORM_FLAGS[@]} \
+  -DCMAKE_CUDA_RUNTIME_LIBRARY=Shared \
+  -DDOUBLE_PRECISION=Yes \
+  -DOUTPUT_BASE_NAME=CyRSoXS_dbl
+make install
+cd ../
+rm -r build-pybind
+
+mkdir build
+cd build
+cmake ${SRC_DIR} \
+  ${CMAKE_ARGS} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DPROFILING=Yes \
+  -DOUTPUT_BASE_NAME=CyRSoXS_pro
+make install
+cd ../
+rm -r build
+
+mkdir build-pybind
+cd build-pybind
+cmake ${SRC_DIR} \
+  ${CMAKE_ARGS} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DPYBIND=Yes \
+  -DUSE_SUBMODULE_PYBIND=No \
+  -DPython_EXECUTABLE="$PYTHON"  \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  ${CMAKE_PLATFORM_FLAGS[@]} \
+  -DCMAKE_CUDA_RUNTIME_LIBRARY=Shared \
+  -DPROFILING=Yes \
+  -DOUTPUT_BASE_NAME=CyRSoXS_pro
+make install
+cd ../
+rm -r build-pybind
+
+mkdir build
+cd build
+cmake ${SRC_DIR} \
+  ${CMAKE_ARGS} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DUSE_64_BIT_INDICES=Yes \
+  -DOUTPUT_BASE_NAME=CyRSoXS_big
+make install
+cd ../
+rm -r build
+
+mkdir build-pybind
+cd build-pybind
+cmake ${SRC_DIR} \
+  ${CMAKE_ARGS} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DPYBIND=Yes \
+  -DUSE_SUBMODULE_PYBIND=No \
+  -DPython_EXECUTABLE="$PYTHON"  \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  ${CMAKE_PLATFORM_FLAGS[@]} \
+  -DCMAKE_CUDA_RUNTIME_LIBRARY=Shared \
+  -DUSE_64_BIT_INDICES=Yes \
+  -DOUTPUT_BASE_NAME=CyRSoXS_big
+make install
+cd ../
+rm -r build-pybind
+
+=======
 # Parallelism (honors conda-forge toolchain)
 export CMAKE_BUILD_PARALLEL_LEVEL="${CPU_COUNT:-1}"
 
@@ -136,3 +244,5 @@ cmake ${SRC_DIR} \
 make install
 cd ../
 rm -r build-pybind
+
+>>>>>>> 22c69c6f0f8babcae96120035e902aa751bc4c76
